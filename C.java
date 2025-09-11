7238de3ed820985acc87b7e697a856a1d3fac62e
TobiasMaugus <tobiasmaugus705@gmail.com>
2025-09-11 16:07:50 -0300

    criacao da classe C

---
+++ C.java
@@ -0,0 +1,12 @@
+public Class C{
+    String C1;
+    int C2;
+
+    public void MC1(){
+        System.out.println("Method C1");
+    }
+
+    public void MC2(){
+        System.out.println("Method C2");
+    }
+}
