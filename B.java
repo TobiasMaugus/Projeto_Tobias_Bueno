59e03bc5b9d11e13c0d164e5df636d2ea18ea948
TobiasMaugus <tobiasmaugus705@gmail.com>
2025-09-11 15:48:26 -0300

    Novas Funcionalidades

---
+++ B.java
@@ -0,0 +1,28 @@
+public Class B{
+    private int B1;
+    private float B2;
+
+    public void MB1(){
+        System.out.println("Method B1");
+    }
+
+    public void MB2(){
+        System.out.println("Method B2");
+    }
+
+    public int get B1(){
+        return B1;
+    }
+
+    public float getB2(){
+        return B2;
+    }
+
+    public void setA1(int b1){
+        B1 = b1;
+    }
+
+    public void setA2(float b2){
+        B2 = b2;
+    }
+}
